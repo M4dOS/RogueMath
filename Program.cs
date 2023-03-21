2b3f78f81c75c58ef130a67c9d27066232273616
MadOS <bondarna2004@mail.ru>
2023-03-21 15:00:45 +1000

    Пустой шаблон проекта

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
