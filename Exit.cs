ff3bec0a1f4a96f03069903f48a6f87f3875ef50
MadOS <bondarna2004@mail.ru>
2023-03-26 18:29:21 +1000

    комната с выходами уже готова (осталась рандомная генерация комнат и их соединение)

--- Exit.cs
+++ Exit.cs
@@ -8,13 +8,16 @@ namespace RogueMath
 {
     internal class Exit
     {
-        protected int x, y; //координаты
-        protected int width; //ширина двери (вероятно не реализуется)
+        public int x, y; //координаты
+        /*protected int width; //ширина двери (вероятно не реализуется)*/
         protected bool isOpen; //открыта ли дверь?
         protected bool isNone; //можно ли наступать на клетку? (1 для пустоты и туннелей, остальное редактируется отдельно)
-        public Exit() 
+        public Exit(int x, int y) 
         {
-            
+            this.x = x;
+            this.y = y;
+            isOpen = true;
+            isNone = true;
         }
     }
 }
--- Room.cs
+++ Room.cs
@@ -14,17 +14,105 @@ namespace RogueMath
 
         public int wigth; //длина
         public int height; //ширина
-        List<Exit> exits; //выходы комнаты
-        bool isExplored; //исследована ли комната?
+        public List<Exit> exits; //выходы комнаты
+        public bool isExplored; //исследована ли комната?
         RoomID roomType; //id комнаты
-        List<CellInfo> objects; //список доп. обьектов на карте
-        public Room(int x, int y, int wight, int height) 
+        public List<CellInfo> objects; //список доп. обьектов на карте
+        public Room(int x, int y, int wight, int height)
         {
+            Random rand = new Random();
             this.x = x;
             this.y = y;
             this.wigth = wight;
             this.height = height;
             this.isExplored = false;
+            this.objects = new List<CellInfo>();
+            this.exits = new List<Exit>();
+            while (exits.Count <= 1)
+            {
+                List<Exit> loc_exits = new List<Exit>();
+
+                for(int i = 0; i < 50000; ++i) 
+                {
+                    Exit exit0 = new Exit(rand.Next(x, x + wight + 1), rand.Next(y, y + height + 1));
+                    loc_exits.Add(exit0);
+                }
+
+                List<Exit> WallN = new List<Exit>();
+                List<Exit> WallS = new List<Exit>();
+                List<Exit> WallW = new List<Exit>();
+                List<Exit> WallE = new List<Exit>();
+
+                foreach (Exit exit in loc_exits)
+                {
+                    if ( (exit.x == x || exit.y == y || exit.x == x + wight-1 || exit.y == y + height -1) //проверка на нахождение на стороне
+                    && (exit.y >= y + 3 && exit.y <= y + height - 4 || exit.x >= x + 3 && exit.x <= x + wight - 4) ) //проверка что это не угол 
+                    {
+                        if (exit.x == x)
+                        {
+                            WallW.Add(exit);
+                        }
+                        else if (exit.y == y)
+                        {
+                            WallN.Add(exit);
+                        }
+                        else if (exit.x == x + wight - 1)
+                        {
+                            WallE.Add(exit);
+                        }
+                        else if (exit.y == y + height - 1)
+                        {
+                            WallS.Add(exit);
+                        }
+                    }
+                }
+
+                //теперь выходы отфильтрованы по стенам
+
+                List<List<Exit>> Walls = new List<List<Exit>> { WallN, WallS, WallW, WallE };
+                foreach (List<Exit> ex in Walls)
+                {
+                    if(ex.Count < 2) //зачем проверять стену с <2 выходами
+                    {
+                        continue;
+                    }
+
+                    while (ex.Count > 2) //нужно не более 2 выходов на 1 стене
+                    {
+                        ex.RemoveAt(0);
+                    }
+
+                    //если расстояние выходов на одной стене маленькое - просто уберу одну из них
+
+                    if ( Math.Abs(ex[0].x - ex[1].x) < 4 && ex == WallN || ex == WallS)
+                    {
+                        ex.RemoveAt(0);
+                    }
+
+                    else if ( Math.Abs(ex[0].y - ex[1].y) < 4 && ex == WallW || ex == WallE)
+                    {
+                        ex.RemoveAt(0);
+                    }
+                }
+                
+                //добавляем все отфильтрованные выходы в один список
+                foreach(List<Exit> ex in Walls)
+                {
+                    foreach (Exit exit in ex)
+                    {
+                        exits.Add(exit);
+                    }
+                }
+
+                //если вышло слишком много, отнимаем до 4
+                if(exits.Count > 4) 
+                { 
+                    while (exits.Count > 4) 
+                    { 
+                        exits.RemoveAt(rand.Next(0,exits.Count));
+                    }
+                }
+            }
         }
     }
 }
