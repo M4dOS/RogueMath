a476f2698be6e694c8c5f1edaee0faa47515543e
MadOS <bondarna2004@mail.ru>
2023-03-31 10:52:44 +1000

    почистил немного код

--- IDs.cs
+++ IDs.cs
@@ -3,11 +3,11 @@ namespace RogueMath
 {
     enum RoomType //id комнаты
     {
-        Bonus = -2,
-        Enemy = -1,
-        Empty = 0,
-        Mather = 1,
-        Shop = 2
+        Bonus = -2, //бонус-комната
+        Enemy = -1, //комната с врагами
+        Empty = 0, //пустая комната (комната спавна она такая)
+        Mather = 1, //комната с боссом
+        Shop = 2 //комната с магазином
     }
     public enum Race
     {
@@ -19,24 +19,29 @@ namespace RogueMath
     enum CellID //id клетки
     {
         //пустота
-        Void = '.',
+        Void = '⋆',
 
-        //стены
+        //по ним нельзя ходить 
         VWall = '│',
         HWall = '—',
         MainVSpot = '0',
         SecondVSpot = '1',
 
-        //другие обьекты
+        //по ним можно ходить
         None = ' ',
         ExitOpen = '+',
         ExitClose = '±',
         Tunel = '#',
 
+        //для классов врагов
         Enemy = 'x',
+
+        //для внутрикартовых обьектов
         Chest = '!',
         Shop = '$',
+
+        //для классов игрока
         Player = '@',
     }
-    
+
 }
